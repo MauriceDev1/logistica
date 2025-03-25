ac098e23ef47393d12a43cd129f58bdf7e1d4dca
MauriceDev1 <mrvolkwyn@gmail.com>
2025-03-25 11:44:57 +0200

    added a new navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -73,6 +73,7 @@ export default function Navbar() {
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               {/* <AuthNav email={user?.email}/> */}
+              d
           </div>
         </div>
       </div>
